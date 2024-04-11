a89691b00e91d0252a92d0ebf3d5c6c8b44703dd
korsapriya <101243771+korsapriya@users.noreply.github.com>
2024-04-11 16:39:03 +0530

    Add files via upload

---
+++ positive nums.py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+
+for x in list1:
+    if x<0:
+        list1.remove(x)
+print(list1)
+
+list2=[12,14,-95,3]
+ 
+for y in list2:
+    if y<0:
+        list2.remove(y)
+print(list2)
